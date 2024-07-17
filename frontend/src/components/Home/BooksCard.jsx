450ba0251883c179a38be7cbf750dcc67bbed10e
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-07-18 02:47:32 +0300

    book store finished course

---
+++ frontend/src/components/Home/BooksCard.jsx
@@ -0,0 +1,13 @@
+import BooksSingleCard from "./BooksSingleCard"
+
+
+const BooksCard = ({books}) => {
+    return (
+        <h1 className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {books.map((item) => {
+                return(<BooksSingleCard key={item._id} book={item}/>)
+            })}
+        </h1>
+    )
+}
+export default BooksCard
